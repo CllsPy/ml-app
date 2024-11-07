4e309d560a6a795324a65917d14e172760fe3563
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-07 21:36:31 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ URL_DATA = 'https://raw.githubusercontent.com/venky14/Machine-Learning-with-Iris
 st.title('Auto-Machine Learning')
 st.info("A webapp based on pure Python that automacaly trains ML models on your data")
 
-with st.expander data:
+with st.expander ('Data'):
   st.write(*Raw Data*)
   df = pd.read_csv(URL_DATA)
   data
