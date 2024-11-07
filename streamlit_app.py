f3764dc4b89f41ae7a9a2611a0c4eeff6a6d952f
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-07 22:39:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 # constants
 URL_DATA = 'https://raw.githubusercontent.com/venky14/Machine-Learning-with-Iris-Dataset/refs/heads/master/Iris.csv'
 st.title('Auto-Machine Learning')
-st.info("A webapp based on pure Python that automacaly trains ML models on your data")
+st.info("Um WEB-APP que automaticamente treina modelos de Machine Learning!")
 
 with st.expander ('Data'):
   st.write('**Raw Data**')
